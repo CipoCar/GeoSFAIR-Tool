9e4e3b9a60b61bda36ac5b58079858794953bb20
Cipolloni Carlo <cipo69carlo@gmail.com>
2026-02-23 10:35:30 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,7 @@
-
+project = 'Nome del tuo Progetto'
+copyright = '2024, Tuo Nome'
+author = 'Tuo Nome'
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
